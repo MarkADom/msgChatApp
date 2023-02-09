fbeca1bdd3b6b1e69e4e2723482cb37487d3de15
Marco Domingues <marco.a.dom78@gmail.com>
2023-02-09 11:07:40 +0000

    User and Main classes

---
+++ src/model/entities/User.java
@@ -0,0 +1,12 @@
+package model.entities;
+
+public class User {
+
+    public String name;
+    public String fullName;
+    public String password;
+    public String email;
+    public String gender;
+    public String phone;
+    
+}
