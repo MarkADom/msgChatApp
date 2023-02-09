41b3b691a4df1d0c6777ec15e74f821041e28864
Marco Domingues <marco.a.dom78@gmail.com>
2023-02-09 22:04:15 +0000

    UserController registration method

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -1,14 +1,17 @@
 package controllers;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-import  model.services.ClientHandler;
+import model.entities.User;
+import model.services.ClientHandler;
 import model.services.Server;
 import javafx.fxml.FXML;
 
 import javax.swing.text.html.ImageView;
+import java.util.ArrayList;
 
 public class UserController {
 
@@ -52,6 +55,65 @@ public class UserController {
     public Label nameExists;
     @FXML
     public Label checkEmail;
+
+    public static String username, password, gender;
+    public static ArrayList<User> loggedInUser = new ArrayList<>();
+    public static ArrayList<User> users = new ArrayList<>();
+
+    public void registration() {
+        if (!regName.getText().equalsIgnoreCase("")
+                && !regPass.getText().equalsIgnoreCase("")
+                && !regEmail.getText().equalsIgnoreCase("")
+                && !regFirstName.getText().equalsIgnoreCase("")
+                && !regPhoneNo.getText().equalsIgnoreCase("")
+                && (male.isSelected()||female.isSelected())){
+            if(checkUser(regName.getText())){
+                if (checkEmail(regEmail.getText())){
+                    User newUser = new User();
+                    newUser.name = regName.getText();
+                    newUser.password = regPass.getText();
+                    newUser.email = regEmail.getText();
+                    newUser.fullName= regFirstName.getText();
+                    newUser.phoneNo = regPhoneNo.getText();
+                    if (male.isSelected()){
+                        newUser.gender = "Male";
+                    } else {
+                        newUser.gender = "Female";
+                    }
+                    users.add(newUser);
+                    goBack.setOpacity(1);
+                    success.setOpacity(1);
+                    makeDefault();
+                    if (controlRegLabel.getOpacity()==1){
+                        controlRegLabel.setOpacity(0);
+                    }
+                    if (nameExists.getOpacity() == 1){
+                        nameExists.setOpacity(0);
+                    }
+                }else {
+                    checkEmail.setOpacity(1);
+                    setOpacity(nameExists, goBack, controlRegLabel, success);
+                }
+            }else {
+                nameExists.setOpacity(1);
+                setOpacity(success, goBack, controlRegLabel, checkEmail);
+            }
+        }else {
+            controlRegLabel.setOpacity(1);
+            setOpacity(success, goBack, nameExists, checkEmail);
+        }
+    }
+
+    private void setOpacity(Label a, Label b, Label c, Label d) {
+        if (a.getOpacity()==1||b.getOpacity()==1||c.getOpacity()==1||d.getOpacity()==1){
+            a.setOpacity(0);
+            b.setOpacity(0);
+            c.setOpacity(0);
+            d.setOpacity(0);
+        }
+    }
+
+
 
 
 }
